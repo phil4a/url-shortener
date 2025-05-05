d59942f1d5c380340153aa0436f99872374ce1aa
Dimitri <philjke@mail.ru>
2025-05-05 23:04:57 +0700

    test(url-exists.pipe): add unit tests for UrlExistsPipe
    Add unit tests to verify the behavior of the UrlExistsPipe, including successful URL retrieval and handling of NotFoundException when the URL is not found

--- src/modules/url/pipes/url-exists/url-exists.pipe.spec.ts
+++ src/modules/url/pipes/url-exists/url-exists.pipe.spec.ts
@@ -1,7 +1,49 @@
+import { DeepMocked, createMock } from '@golevelup/ts-jest';
+import { NotFoundException } from '@nestjs/common';
+import { Url } from 'prisma/generated/prisma/client';
+import { UrlService } from '../../url.service';
 import { UrlExistsPipe } from './url-exists.pipe';
 
+// Мокаем модуль, который вызывает ошибку
+jest.mock(
+  'prisma/generated/prisma/client',
+  () => ({
+    PrismaClient: jest.fn().mockImplementation(() => ({})),
+  }),
+  { virtual: true },
+);
+
 describe('UrlExistsPipe', () => {
+  let urlExistsPipe: UrlExistsPipe;
+  let urlService: DeepMocked<UrlService>;
+
+  beforeEach(async () => {
+    urlService = createMock<UrlService>();
+    urlExistsPipe = new UrlExistsPipe(urlService);
+  });
+
   it('should be defined', () => {
-    expect(new UrlExistsPipe()).toBeDefined();
+    expect(urlExistsPipe).toBeDefined();
+  });
+
+  it('should return redirectUrl', async () => {
+    let url: Url = {
+      id: 1,
+      redirect: 'https://instagram.com',
+      url: 'localhost:3000/random-url',
+      title: 'Instagram',
+      clicks: 0,
+      description: 'Instagram',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    urlService.findOne.mockResolvedValue(url);
+    const result = await urlExistsPipe.transform('random-url');
+    expect(result).toBe(url);
+  });
+  it('should return 404-exception if url not found', async () => {
+    urlService.findOne.mockResolvedValue(null);
+    const result = async () => await urlExistsPipe.transform('random-url');
+    expect(result).rejects.toThrow(NotFoundException);
   });
 });
