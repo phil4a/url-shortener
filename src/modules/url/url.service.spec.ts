140fbefab1f62f7b200446a97e62aff4549a5dc3
phil <philjke@mail.ru>
2025-05-06 11:06:43 +0700

    test(url): add tests for findAll, findOne, and incrementClicks methods
    This commit introduces new unit tests for the `findAll`, `findOne`, and `incrementClicks` methods in the `UrlService`. The tests ensure that these methods behave as expected, including proper handling of pagination, fetching a single URL, and incrementing click counts.

--- src/modules/url/url.service.spec.ts
+++ src/modules/url/url.service.spec.ts
@@ -33,7 +33,7 @@ describe('UrlService', () => {
   let configService: DeepMocked<ConfigService>;
   let databaseService: DeepMockProxy<DatabaseService>;
   let host = 'localhost:3000';
-  // let paginationService: DeepMocked<PaginationService>;
+  let paginationService: DeepMockProxy<PaginationService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -65,7 +65,7 @@ describe('UrlService', () => {
     configService = module.get(ConfigService);
     configService.getOrThrow.mockReturnValue(host);
     databaseService = module.get(DatabaseService);
-    // paginationService = module.get(PaginationService)
+    paginationService = module.get(PaginationService);
 
     await app.init();
   });
@@ -95,6 +95,121 @@ describe('UrlService', () => {
       const result = await urlService.create(payload);
 
       expect(result).toEqual(mockedUrl);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all urls with pagination', async () => {
+      const filter = 'test';
+      const page = 2;
+      const limit = 10;
+
+      const mockedUrls = [
+        {
+          id: 1,
+          redirect: 'https://test1.com',
+          title: 'Test 1',
+          description: 'Test description 1',
+          clicks: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          url: `${host}/test1`,
+        },
+        {
+          id: 2,
+          redirect: 'https://test2.com',
+          title: 'Test 2',
+          description: 'Test description 2',
+          clicks: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          url: `${host}/test2`,
+        },
+      ];
+
+      const paginationResult = {
+        meta: {
+          totalCount: mockedUrls.length,
+          currentPage: page,
+          totalPages: 1,
+          nextPage: 'https://test2.com/page1',
+          prevPage: 'https://test2.com/page3',
+          perPage: limit,
+        },
+        urls: mockedUrls,
+      };
+
+      paginationService.getPagination.mockResolvedValue(paginationResult);
+
+      const result = await urlService.findAll({
+        filter,
+        page,
+        limit,
+      });
+
+      expect(result).toEqual({
+        data: mockedUrls,
+        meta: paginationResult.meta,
+      });
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a url', async () => {
+      const payload = {
+        redirect: 'https://instagram.com',
+        title: 'Instagram',
+        description: 'Social media',
+      };
+      const uid = '12345';
+      const mockedUrl = {
+        ...payload,
+        id: 1,
+        clicks: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        url: `${host}/${uid}`,
+      };
+
+      databaseService.url.findUnique.mockResolvedValueOnce(mockedUrl);
+
+      const result = await urlService.findOne(uid);
+
+      expect(result).toEqual(mockedUrl);
+    });
+  });
+
+  describe('incrementClicks', () => {
+    it('should increment clicks', async () => {
+      // Подготовка начальных данных
+      const urlId = 1;
+      const initialUrl = {
+        id: urlId,
+        redirect: 'https://instagram.com',
+        title: 'Instagram',
+        description: 'Social media',
+        clicks: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        url: `${host}/12345`,
+      };
+
+      const updatedUrl = {
+        ...initialUrl,
+        clicks: 6, // Увеличено на 1
+      };
+
+      databaseService.url.update.mockResolvedValueOnce(updatedUrl);
+
+      const result = await urlService.incrementClicks(urlId);
+
+      expect(databaseService.url.update).toHaveBeenCalledWith({
+        where: { id: urlId },
+        data: { clicks: { increment: 1 } },
+      });
+
+      expect(result).toEqual(updatedUrl);
+      expect(result.clicks).toBe(initialUrl.clicks + 1);
     });
   });
 });
